2fa65b76d70e3e5f75ef6d74ac527f5f52c1f0f1
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-09-05 20:39:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+function Rectangle(sides, width, height) {
+  this.sides = sides;
+  this.width = width;
+  this.height = height;
+  this.area = function() {
+    return this.width * this.height;
+  }
+  this.perimeter = function() {
+    return (this.width + this.height) * 2;
+  }
+}
+ 
+// Square constructor
+function Square(sides, length) {
+  this.sides = sides;
+  this.width = length;
+  this.height = length;
+  this.area = function() {
+    return this.width * this.height;
+  },
+  this.perimeter = function() {
+    return (this.width + this.height) * 2;
+  }
+}
+ 
+var rect = new Rectangle(4, 3, 5);
+var square = new Square(4, 2);
+ 
+rect.area();
+square.area();
